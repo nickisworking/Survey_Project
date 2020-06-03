260d00b0c41c62b34fa58b2a81dd9ed63c795ac8
minseung.Shin <alstmd1542@gmail.com>
2020-06-03 16:28:09 +0900

    address

--- routes/signin.js
+++ routes/signin.js
@@ -6,7 +6,7 @@ const mysql = require('mysql')
 const connection= mysql.createConnection({
   host : 'localhost',
   user : 'root',
-  password : 'gmdrb918@@',
+  password : 'wlfkf12!!',
   database : 'survey'
 });
 
